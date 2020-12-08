08c3e55511995850ab5f4cda25d237f693f53adb
Alexey Volkov <asgalex@gmail.com>
2020-12-09 02:47:44 +0300

    heroku integration

--- bin/runserver-heroku.sh
+++ bin/runserver-heroku.sh
@@ -2,4 +2,4 @@
 
 echo "command: dart --no-sound-null-safety run bin/server.dart --botKey $botKey -u $u -a $a -m $m -r $r -i $i"
 
-dart --no-sound-null-safety run bin/server.dart --botKey $botKey -u $u -a $a -m $m -r $r -i $i
+dart --no-sound-null-safety run bin/server.dart --botKey "$botKey" -u "$u" -a "$a" -m "$m" -r "$r" -i "$i"
