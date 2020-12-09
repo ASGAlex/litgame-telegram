62eb6594ca6958e7e2509e89c0f68e672141b9e4
Alexey Volkov <asgalex@gmail.com>
2020-12-09 12:56:29 +0300

    redundant file

--- bin/runserver-heroku.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "debug"
-echo "command: dart --no-sound-null-safety run bin/server.dart --botKey $botKey -u $u -a $a -m $m -r $r -i $i"
-
-#dart --no-sound-null-safety run bin/server.dart --botKey "$botKey" -u "$u" -a "$a" -m "$m" -r "$r" -i "$i"
